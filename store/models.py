85ff688f0f7ce3022066844e2a3df785afa7cacd
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-25 21:17:43 +0530

    Add StoreOrder model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('0', 'New'),
+    ('1', 'In Process'),
+    ('2', 'Stored'),
+    ('3', 'Send'),
+)
+
+
+class StoreOrder(models.Model):
+    order_number = models.CharField(max_length=50, unique=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='0')
+
+    def __str__(self):
+        return str(self.order_number)
