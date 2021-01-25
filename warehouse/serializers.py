5303315b57e739a36ab00c38d709fb3da40f8350
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-26 01:30:12 +0530

    Add WarehouseOrder Serializer

---
+++ warehouse/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import WarehouseOrder
+
+
+class WarehouseOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WarehouseOrder
+        fields = ['order_number', 'status']
