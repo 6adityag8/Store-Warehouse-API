1e4de4611ec3ff241dab7c97faffc691f0017734
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-26 01:33:07 +0530

    Call the API to add Warehouse while adding Store to DB

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,19 @@
+import requests
 from django.contrib import admin
+from rest_framework import status
+from rest_framework.reverse import reverse
 
 from .models import StoreOrder
 
-admin.site.register(StoreOrder)
+
+@admin.register(StoreOrder)
+class StoreOrderAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        data = {
+            'status': obj.status,
+            'order_number': obj.order_number,
+        }
+        response = requests.post(reverse('add_warehouse', request=request), data=data)
+        if int(response.status_code) == status.HTTP_201_CREATED:
+            super().save_model(request, obj, form, change)
