f5a2a7a78cbeacdaa2e20689f72fef1f33930b83
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-25 21:35:18 +0530

    Add WarehouseOrder model

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from store_warehouse_api.constants import STATUS_CHOICES
+
+
+class WarehouseOrder(models.Model):
+    order_number = models.CharField(max_length=50)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='0')
+
+    def __str__(self):
+        return str(self.order_number)
